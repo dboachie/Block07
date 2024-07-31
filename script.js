49f1c361df75992ea21c2f2937cfecb76f3eb9a9
Daniel Boachie <dboachie222@gmail.com>
2024-07-31 15:23:29 -0400

    Initial

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+
+// Steps to solve exercise
+// 1. Use console log to create string for user saying why the user has been chosen along with the secret combination.
+// 2. Create variables for the three numbers in the combination.
+// 3. Use an alert to show the secret combination.
+
+
+
+// Console log to create string for user saying why the user has been chosen along with the secret combination
+console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+
+
+
+
+// Create variables for the three numbers in the combination.
+
+const x = 5 * 2
+
+const y = 25 + 15
+
+const z = 50 - 11
+
+
+
+
+// Alert to show the secret combination
+alert("10 - 40 - 39")
+
+
